4ef5ce60e91ed3d61bf8456ca6a48910f48895e3
Xenon Ollie Zoidberg <xoz@zoidberg.tech>
2026-09-16 10:39:57 -0700

    Added J aliased to | jq

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias cls='clear' # Good 'ol Clear Screen command
 alias -g B='; printf "\a"' # Ring terminal bell, e.g.: long-cmd B
+alias -g J='| jq' # pretty json
 grel() { git commit -a -m "$2" -n && git tag -a "$1" -m "$2"; }
